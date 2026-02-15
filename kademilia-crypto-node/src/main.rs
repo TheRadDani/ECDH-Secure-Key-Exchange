a0b8685cdc7161d294e267f514f68e687216ebdb
TheRadDani <ldanielfch@gmail.com>
2026-02-15 20:03:07 +0000

    Add code to main source file to add libraries requiredd for the protocol implementation

--- kademilia-crypto-node/src/main.rs
+++ kademilia-crypto-node/src/main.rs
@@ -1,3 +1,18 @@
+use anyhow::{Context, Result};
+use lilp2p::{
+    core::upgrade,
+    identity,
+    kad::{self, store::MemoryStore, Behaviour as KademliaBehaviour, Config as KademliaConfig},
+    noise,
+    swarm::{NetworkBehavious, SwarmBuilder, SwarmEvent},
+    tcp, yamux, Multiaddr, PeerId, Transport,
+};
+use serde::{Deserialize, Serialize};
+use sha3::{Digest, Keccak256};
+use std::io::{self, BufRead};
+use std::time::Duration;
+use tokio::time::sleep;
+
 fn main() {
     println!("Hello, world!");
 }
